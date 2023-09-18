76004a25445e9b01f736d2523ee93300f62cf738
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-09-18 18:04:52 +0000

    feat: declare api, add HttpClient to constructor

--- myFlix-Angular-client/src/app/fetch-api-data.service.spec.ts
+++ myFlix-Angular-client/src/app/fetch-api-data.service.spec.ts
@@ -2,6 +2,41 @@ import { TestBed } from '@angular/core/testing';
 
 import { FetchApiDataService } from './fetch-api-data.service';
 
+import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs';
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+// declare api url that will fetch data
+const apiUrl = 'https://movies-app1-3d6bd65a6f09.herokuapp.com';
+@Injectable({
+  providedIn: 'root',
+})
+export class userRegistrationService {
+  constructor(private http: HttpClient) {}
+  public userRegistration(userDetails: any): Observable<any> {
+    console.log(userDetails);
+    return this.http
+      .post(apiUrl + 'users', userDetails)
+      .pipe(catchError(this.handleError));
+  }
+  private handleError(error: HttpErrorResponse): any {
+    if (error.error instanceof ErrorEvent) {
+      console.error('Some error occurred: ', error.error.message);
+    } else {
+      console.error(
+        `Error Status code ${error.status}, ` + `Error body is: ${error.error}`
+      );
+    }
+    return throwError('Something bad happened. Please try again later.');
+  }
+}
+
 describe('FetchApiDataService', () => {
   let service: FetchApiDataService;
 
--- myFlix-Angular-client/src/app/fetch-api-data.service.ts
+++ myFlix-Angular-client/src/app/fetch-api-data.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
