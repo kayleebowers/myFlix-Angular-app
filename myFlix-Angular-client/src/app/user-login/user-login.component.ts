30b631f066f23ad7ce025cfd668a0c849353d4a8
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-09-19 23:29:18 +0000

    feat: fetch data in UserLoginComponent

--- myFlix-Angular-client/src/app/user-login/user-login.component.ts
+++ myFlix-Angular-client/src/app/user-login/user-login.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-user-login',
   templateUrl: './user-login.component.html',
   styleUrls: ['./user-login.component.scss']
 })
-export class UserLoginComponent {
+export class UserLoginComponent implements OnInit {
+  @Input() userLogin = { Username: "", Password: "", Token: ""};
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserLoginComponent>,
+    public snackBar: MatSnackBar) {}
+  
+  ngOnInit(): void {
+  }
 
+  loginUser(): void {
+    this.fetchApiData.userLogin(this.userLogin).subscribe((result) => {
+      this.dialogRef.close();
+      this.snackBar.open(result, "OK", {
+        duration: 2000
+      });
+    }, (result) => {
+      this.snackBar.open(result, 'OK', {
+        duration: 2000
+      });
+    });
+  }
 }
