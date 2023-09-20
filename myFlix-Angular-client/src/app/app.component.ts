411a91930387ed3ea8bcffa4b83ea0f39f14d2b9
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-09-20 02:03:32 +0000

    feat: transfer login/registration to welcome pg

--- myFlix-Angular-client/src/app/app.component.ts
+++ myFlix-Angular-client/src/app/app.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
-import { MatDialog } from '@angular/material/dialog';
-import { UserLoginComponent } from './user-login/user-login.component';
-import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @Component({
   selector: 'app-root',
@@ -11,27 +7,4 @@ import { MovieCardComponent } from './movie-card/movie-card.component';
 })
 export class AppComponent {
   title = 'myFlix-Angular-client';
-
-  constructor(public dialog: MatDialog) { }
-
-  // open registration dialog on button click
-  openUserRegistrationDialog(): void {
-    this.dialog.open(UserRegistrationFormComponent, {
-      width: "280px"
-    });
-  }
-
-  // open login dialog on button click
-  openLoginDialog(): void {
-    this.dialog.open(UserLoginComponent, {
-      width: "280px"
-    })
-  }
-
-  // open all movies dialog
-  openMoviesDialog(): void {
-    this.dialog.open(MovieCardComponent, {
-      width: "500px"
-    })
-  }
 }
