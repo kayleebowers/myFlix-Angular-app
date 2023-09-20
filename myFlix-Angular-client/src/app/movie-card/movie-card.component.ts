817bf866276f7e1b742d15b5c11a98c11ac05bb4
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-09-20 16:48:58 +0000

    feat: add opening genre dialog

--- myFlix-Angular-client/src/app/movie-card/movie-card.component.ts
+++ myFlix-Angular-client/src/app/movie-card/movie-card.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service';
 import { MatDialog } from '@angular/material/dialog';
 import { MovieDetailsComponent } from "../movie-details/movie-details.component";
+import { MovieGenreComponent } from '../movie-genre/movie-genre.component';
 
 @Component({
   selector: 'app-movie-card',
@@ -28,11 +29,17 @@ export class MovieCardComponent {
     })
   }
 
+  // functions to open movie info dialogs 
   openMovieDetailsDialog(): void {
     this.dialog.open(MovieDetailsComponent, {
       width: "500px"
     })
   }
 
-  
+  openGenreDialog(): void {
+    this.dialog.open(MovieGenreComponent, {
+      width: "500px"
+    });
+  }
+
 }
