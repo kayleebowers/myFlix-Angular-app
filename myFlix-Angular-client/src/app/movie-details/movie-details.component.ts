3a5b923ce5000c2577941d554d6320dece9ee912
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-09-21 12:52:03 -0500

    Revert "fix: reinstall github pages"

---
+++ myFlix-Angular-client/src/app/movie-details/movie-details.component.ts
@@ -0,0 +1,16 @@
+import { Component, Inject } from '@angular/core';
+import {MAT_DIALOG_DATA} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-movie-details',
+  templateUrl: './movie-details.component.html',
+  styleUrls: ['./movie-details.component.scss']
+})
+export class MovieDetailsComponent {
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      Title: string,
+      Summary: string
+    }) {}
+}
